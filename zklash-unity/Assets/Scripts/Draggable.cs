81b56f8961215601d88685a19e34abe3ea51aefe
ClanCo <corentin.terrie@gmail.com>
2024-04-05 10:16:25 +0200

    First it of scene

---
+++ zklash-unity/Assets/Scripts/Draggable.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class Draggable : MonoBehaviour
+{
+    bool drag;
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Mouse down");
+        drag = true;
+    }
+
+    private void OnMouseUp()
+    {
+        Debug.Log("Mouse up");
+        drag = false;
+    }
+
+    private void Update()
+    {
+        if (drag)
+        {
+            Debug.Log("Dragging");
+            Vector2 MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            transform.Translate(MousePos);
+        }
+    }
+
+}

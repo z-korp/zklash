ecae344d4d1adb2407e08c0f46b3039ab17f7b82
ClanCo <corentin.terrie@gmail.com>
2024-04-06 14:09:39 +0200

    Droppable Zone

--- zklash-unity/Assets/Scripts/Draggable.cs
+++ zklash-unity/Assets/Scripts/Draggable.cs
@@ -4,35 +4,62 @@ using UnityEngine;
 public class Draggable : MonoBehaviour
 {
     bool drag;
+    public Vector3 initPos = Vector3.zero;
 
     private Rigidbody2D rb;
+    private DroppableZone currentDroppableZone;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-    }
-    private void OnMouseDown()
-    {
-        Debug.Log("Mouse down");
-        drag = true;
-    }
-
-    private void OnMouseUp()
-    {
-        Debug.Log("Mouse up");
-        drag = false;
     }
 
     private void Update()
     {
         if (drag)
         {
-            Debug.Log("Dragging");
-            //Vector2 MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-            //transform.Translate(MousePos);
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             rb.MovePosition(mousePos);
 
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        drag = true;
+        initPos = transform.position;
+    }
+
+    private void OnMouseUp()
+    {
+        drag = false;
+        if (currentDroppableZone != null && currentDroppableZone.CanBeDropped())
+        {
+            Debug.Log("Objet déposé dans la zone droppable.");
+        }
+        else
+        {
+            rb.MovePosition(initPos);
+            Debug.Log("Objet non déposé.");
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        DroppableZone zone = collision.GetComponent<DroppableZone>();
+        if (zone != null)
+        {
+            currentDroppableZone = zone;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        DroppableZone zone = collision.GetComponent<DroppableZone>();
+        if (zone == currentDroppableZone)
+        {
+            currentDroppableZone = null;
         }
     }
 
--- zklash-unity/Assets/Scripts/DroppableZone.cs
+++ zklash-unity/Assets/Scripts/DroppableZone.cs
@@ -2,25 +2,31 @@ using UnityEngine;
 
 public class DroppableZone : MonoBehaviour
 {
+    private bool isDroppable;
+
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            Debug.Log("Tuile déplaçable entrée dans la zone de dépôt");
+            Debug.Log("Player entered droppable zone");
+            isDroppable = true;
         }
 
-        Debug.Log("Tuile déplaçable entrée dans la zone de dépôt 2");
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         Draggable draggable = collision.gameObject.GetComponent<Draggable>();
 
-        // Vérifiez si l'objet sortant est une tuile déplaçable
         if (draggable != null)
         {
-            Debug.Log("Tuile déplaçable sortie de la zone de dépôt");
-            // Vous pouvez ajouter ici la logique pour gérer la tuile déplaçable lorsqu'elle quitte la zone
+            isDroppable = false;
         }
+    }
+
+    public bool CanBeDropped()
+    {
+        return isDroppable;
     }
 }

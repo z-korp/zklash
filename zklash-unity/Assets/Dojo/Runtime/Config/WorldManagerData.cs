6e3059a7d774b397805b42b608d546f042e81344
Cheelax <thomas.belloc@gmail.com>
2024-04-07 20:33:30 +0200

    update var

--- zklash-unity/Assets/Dojo/Runtime/Config/WorldManagerData.cs
+++ zklash-unity/Assets/Dojo/Runtime/Config/WorldManagerData.cs
@@ -8,12 +8,12 @@ namespace Dojo
     public class WorldManagerData : ScriptableObject
     {
         [Header("RPC")]
-        public string toriiUrl = "http://localhost:8080";
-        public string rpcUrl = "http://localhost:5050";
+        public string toriiUrl = "https://api.cartridge.gg/x/zklash/torii";
+        public string rpcUrl = "https://api.cartridge.gg/x/zklash/katana";
         public string relayUrl = "/ip4/127.0.0.1/tcp/9090";
         public string relayWebrtcUrl;
         public uint limit = 100;
         [Header("World")]
-        public string worldAddress;
+        public string worldAddress = "0x11aa9b474f093e27b773aaec3c0bae571140e825d27ac433698d3584c8d4af9";
     }
 }

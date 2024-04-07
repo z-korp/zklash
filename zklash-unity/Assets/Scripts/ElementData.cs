2377497a03f275c9cd80d34c6db8339a95eb1729
ClanCo <corentin.terrie@gmail.com>
2024-04-07 09:56:30 +0200

    PowerUp

--- zklash-unity/Assets/Scripts/ElementData.cs
+++ zklash-unity/Assets/Scripts/ElementData.cs
@@ -12,17 +12,20 @@ public class ElementData : MonoBehaviour
     public int maxHealth = 10;
     public int currentHealth = 3;
 
+    public int currentDamage = 2;
+
     public Animator animator;
 
     public SpriteRenderer spriteRenderer;
 
     public TextMeshProUGUI txtLife;
+    public TextMeshProUGUI txtAttack;
 
     public GameObject vfxHandler;
 
-    public bool isInvicible = false;
-    public float invicibilityBlinkDuration = 0.2f;
-    public float invicibilityTimeAfterHit = 2f;
+    public bool isBlinking = false;
+    public float blinkDuration = 0.2f;
+    public float blinkTimeAfterHit = 1f;
 
 
 
@@ -56,6 +59,10 @@ public class ElementData : MonoBehaviour
         {
             DealDamage(1);
         }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            PowerUp(1);
+        }
     }
 
     public void TakeDamage(int amount)
@@ -74,33 +81,15 @@ public class ElementData : MonoBehaviour
         }
         else
         {
-            if (gameObject.CompareTag("Enemy"))
-            {
-                SetTextHealth(currentHealth);
-            }
-            isInvicible = true;
-            StartCoroutine(InvincibilityFlash());
-            StartCoroutine(HandleInvincibilityDelay());
+
+            SetTextHealth(currentHealth);
+
+            isBlinking = true;
+            StartCoroutine(BlinkDamageFlash());
+            StartCoroutine(HandleBlinkDelay());
             // TBD: Update text from health bar
             //animator.SetTrigger("IsHurt");
         }
-    }
-
-    public IEnumerator InvincibilityFlash()
-    {
-        while (isInvicible)
-        {
-            spriteRenderer.color = new Color(1f, 0f, 0f, 1f);
-            yield return new WaitForSeconds(invicibilityBlinkDuration);
-            spriteRenderer.color = new Color(1f, 1f, 1f, 1f);
-            yield return new WaitForSeconds(invicibilityBlinkDuration);
-        }
-    }
-
-    public IEnumerator HandleInvincibilityDelay()
-    {
-        yield return new WaitForSeconds(invicibilityTimeAfterHit);
-        isInvicible = false;
     }
 
     public void DealDamage(int amount)
@@ -112,6 +101,17 @@ public class ElementData : MonoBehaviour
     public void StopAnimAttack()
     {
         animator.SetTrigger("Attack");
+    }
+
+    public void PowerUp(int amount)
+    {
+
+        currentDamage += amount;
+        SetTextHealth(currentDamage);
+
+        isBlinking = true;
+        StartCoroutine(BlinkPowerUpFlash());
+        StartCoroutine(HandleBlinkDelay());
     }
 
     public void HealPlayer(int amount)
@@ -126,6 +126,9 @@ public class ElementData : MonoBehaviour
         }
         SetTextHealth(currentHealth);
 
+        isBlinking = true;
+        StartCoroutine(BlinkHealFlash());
+        StartCoroutine(HandleBlinkDelay());
     }
 
     public void Death()
@@ -136,5 +139,43 @@ public class ElementData : MonoBehaviour
     public void SetTextHealth(int amount)
     {
         txtLife.text = amount.ToString();
+    }
+
+    public IEnumerator BlinkPowerUpFlash()
+    {
+        while (isBlinking)
+        {
+            spriteRenderer.color = new Color(0f, 0f, 1f, 1f);
+            yield return new WaitForSeconds(blinkDuration);
+            spriteRenderer.color = new Color(1f, 1f, 1f, 1f);
+            yield return new WaitForSeconds(blinkDuration);
+        }
+    }
+    public IEnumerator BlinkHealFlash()
+    {
+        while (isBlinking)
+        {
+            spriteRenderer.color = new Color(0f, 1f, 0f, 1f);
+            yield return new WaitForSeconds(blinkDuration);
+            spriteRenderer.color = new Color(1f, 1f, 1f, 1f);
+            yield return new WaitForSeconds(blinkDuration);
+        }
+    }
+
+    public IEnumerator BlinkDamageFlash()
+    {
+        while (isBlinking)
+        {
+            spriteRenderer.color = new Color(1f, 0f, 0f, 1f);
+            yield return new WaitForSeconds(blinkDuration);
+            spriteRenderer.color = new Color(1f, 1f, 1f, 1f);
+            yield return new WaitForSeconds(blinkDuration);
+        }
+    }
+
+    public IEnumerator HandleBlinkDelay()
+    {
+        yield return new WaitForSeconds(blinkTimeAfterHit);
+        isBlinking = false;
     }
 }

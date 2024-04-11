58c7f5f61f6e819d4e07f5ed1274f552ed7f3475
bal7hazar <bal7hazar@proton.me>
2024-04-11 15:17:34 +0200

    🔇 Remove logs

--- app/Assets/Scripts/ShopManager.cs
+++ app/Assets/Scripts/ShopManager.cs
@@ -23,17 +23,11 @@ public class ShopManager : MonoBehaviour
 
     public int IndexOfMobSpawn(MobSpawn _spawn)
     {
-        Debug.Log("IndexOf");
-        Debug.Log("shopMobs: " + shopMobs.Length);
         for (int i = 0; i < shopMobs.Length; i++)
         {
             MobSpawn spawn = shopMobs[i].GetComponent<MobSpawn>();
-            Debug.Log("Current: " + shopMobs[i]);
-            Debug.Log("Spawn: " + spawn);
-            Debug.Log("Obj: " + _spawn);
             if (spawn == _spawn)
             {
-                Debug.Log("Mob: " + i);
                 return i;
             }
         }
@@ -42,16 +36,11 @@ public class ShopManager : MonoBehaviour
 
     public int IndexOfItemSpawn(ItemSpawn _spawn)
     {
-        Debug.Log("IndexOf");
         for (int i = 0; i < shopItems.Length; i++)
         {
             ItemSpawn spawn = shopItems[i].GetComponent<ItemSpawn>();
-            Debug.Log("Current: " + shopItems[i]);
-            Debug.Log("Spawn: " + spawn);
-            Debug.Log("Obj: " + _spawn);
             if (spawn == _spawn)
             {
-                Debug.Log("Item: " + i);
                 return i;
             }
         }

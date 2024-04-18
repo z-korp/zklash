a839e7bb0ae1602a695705cefe7f8f9ad7123663
ClanCo <corentin.terrie@gmail.com>
2024-04-18 17:36:14 +0200

    Load Item on mob

--- app/Assets/Scripts/Mob/MobItem.cs
+++ app/Assets/Scripts/Mob/MobItem.cs
@@ -3,5 +3,36 @@ using UnityEngine;
 public class MobItem : MonoBehaviour
 {
     public ItemData item;
+    private ItemData previousItem;
+
+
+    public int health;
+    public int attack;
+    public int damage;
+    public int absorb;
+    public int save;
+    public int durability;
+    private void Update()
+    {
+
+        if (item != previousItem)
+        {
+            Debug.Log("Item changed");
+            previousItem = item;
+            UpdateGameObjectWithItemData();
+        }
+    }
+    private void UpdateGameObjectWithItemData()
+    {
+        if (item != null)
+        {
+            health = item.health;
+            attack = item.attack;
+            damage = item.damage;
+            absorb = item.absorb;
+            save = item.save;
+            durability = item.durability;
+        }
+    }
 
 }

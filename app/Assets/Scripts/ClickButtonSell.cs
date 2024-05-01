0cbb51479fef2de5180c026c1bf26100d7b78a06
ClanCo <corentin.terrie@gmail.com>
2024-05-01 12:21:13 +0200

    Sell mob btn

--- app/Assets/Scripts/ClickButtonSell.cs
+++ app/Assets/Scripts/ClickButtonSell.cs
@@ -4,10 +4,19 @@ using UnityEngine.UI;
 using System.Linq;
 using System;
 
-public class ClickButtonSell : MonoBehaviour
+public class ClickButtonSell : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Sell Mob !");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("En fait non!");
+    }
     public void OnClickSell(uint character_id)
     {
-       //ContractActions.instance.TriggerSell(character_id);
+        //ContractActions.instance.TriggerSell(character_id);
     }
 }

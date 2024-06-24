b493739de07f86ef2bbcd399a0c5275114bf0e55
Matthias <matthias.monnier@gmail.com>
2024-06-24 11:31:13 +0200

    Update react unity

--- web-unity/src/App.tsx
+++ web-unity/src/App.tsx
@@ -1,18 +1,15 @@
-import { useDojo } from './dojo/useDojo';
-import './App.css';
-import UnityLoader from './ui/components/UnityLoader';
-import Connect from './ui/components/Connect';
+import UnityLoader from "./ui/components/UnityLoader";
+import { Header } from "./ui/containers/Header";
 
 function App() {
-  const {
-    account: { account },
-  } = useDojo();
-
   return (
-    <div className="flex flex-col gap-4 w-full items-center mt-20">
-      {/*<Connect />*/}
-
-      <UnityLoader />
+    <div className="relative flex flex-col w-screen">
+      <Header />
+      <div className="relative flex flex-col gap-8 grow items-center justify-start">
+        <div className="absolute top-0 flex flex-col items-center gap-4 w-full px-4 py-2 max-w-4xl mx-auto">
+          <UnityLoader />
+        </div>
+      </div>
     </div>
   );
 }

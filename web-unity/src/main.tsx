cc43d46f29ef4b72adc080676e40e4f1ca08b93a
Matthias <matthias.monnier@gmail.com>
2024-06-24 15:41:57 +0200

    update unity version

--- web-unity/src/main.tsx
+++ web-unity/src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { Toaster } from "@/ui/elements/sonner.tsx";
 import {
   StarknetConfig,
   argent,
